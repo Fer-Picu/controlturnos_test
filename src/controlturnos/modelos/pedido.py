ca4fff884cdfe00df2496c586f93ab68790a5d2d
Fer-Picu <piaggi.fer@gmail.com>
2013-04-19 16:53:48 -0300

    final del dia

--- src/controlturnos/modelos/pedido.py
+++ src/controlturnos/modelos/pedido.py
@@ -1,12 +1,17 @@
 import grok
-
+from datetime import datetime
 grok.templatedir("templates")
 
 
 class Pedido(grok.Model):
-    pass
+    def __init__(self):
+        super(Pedido, self).__init__()
+        self.now = datetime.now().strftime('%Y-%m-%d %H:%M')
+        self.lista = range(4)
 
 
 class PedidoIndex(grok.View):
     grok.name("index")
-    pass
+
+    def update(self):
+        self.context.now = datetime.now().strftime('%Y-%m-%d %H:%M')
