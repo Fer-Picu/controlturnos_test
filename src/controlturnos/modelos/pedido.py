c681693fa455c9a46819c4058ba4e3e2a3f8b604
Fer-Picu <piaggi.fer@gmail.com>
2013-04-22 10:54:40 -0300

    intento de schemas y forms

--- src/controlturnos/modelos/pedido.py
+++ src/controlturnos/modelos/pedido.py
@@ -1,13 +1,35 @@
 import grok
 from datetime import datetime
+from zope import schema
+from zope.interface import Interface
+
 grok.templatedir("templates")
 
 
+class Ipedido(Interface):
+    nombre = schema.TextLine(
+                    title=u"Nombre",
+                    description=u"Nombre de la seccion",
+                    required=True)
+    pass
+
+
 class Pedido(grok.Model):
+    grok.implements(Ipedido)
+    title = u''
+    description = u''
+
     def __init__(self):
         super(Pedido, self).__init__()
         self.now = datetime.now().strftime('%Y-%m-%d %H:%M')
         self.lista = range(4)
+
+
+class PedidoForm(grok.Form):
+    grok.context(Pedido)
+    grok.name('index')
+    form_field = grok.AutoFields(Pedido)
+    pass
 
 
 class PedidoIndex(grok.View):
